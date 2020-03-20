db3714d4a0fae68de0908789776565571217e8dd
Paulo César Benachio <paulo.cesar@zoly.com.br>
2020-03-20 03:17:43 -0300

    FINALIZADA | GA - Botão Flutuante - View Source

--- templates/android-dot-com/desafio2.js
+++ templates/android-dot-com/desafio2.js
@@ -1,10 +1,14 @@
 //Desafio 2.0
 
+//AQUI EU PREFERI CAPTURAR O ELEMENTO HTML USANDO UM ATRIBUTO AO INVÉS DA CLASSE PARA JÁ PRATICAR O USO:
 var viewSource = document.querySelector("[teste]");
 
+//ADICIONO UM ESCUTADOR NO ELEMENTO QUE AO SER CLICADO IRÁ COLOCAR A VARIÁVEL E O EVENTO NO DATALAYER:
 viewSource.addEventListener("click", function(){
 	dataLayer.push({
+		//VARIÁVEL:
 		"tipo_botao":"view_source",
+		//EVENTO:
 		"event":"disparo-personalizado"
 
 	});

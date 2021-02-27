b5c7b0570e42f7f8ebfbf07cc35cbb8182536869
pcbenachio <pcbenachio@gmail.com>
2021-02-27 02:03:20 -0300

    Implementação Evento Logo

--- templates/android-dot-com/desafio2.js
+++ templates/android-dot-com/desafio2.js
@@ -1,15 +1,11 @@
 //Desafio 2.0
 
 //CAPTURANDO O ELEMENTO HTML USANDO UM ATRIBUTO AO INVÉS DA CLASSE PARA PRATICAR O USO DO DATA ATTRIBUTE:
-var viewSource = document.querySelector("[teste]");
+var logo = document.querySelector(".android-logo-image");
 
 //ADICIONANDO UM ESCUTADOR NO ELEMENTO DO BOTÃO VIEW SOURCE, QUE AO SER CLICADO IRÁ COLOCAR A VARIÁVEL E O EVENTO NO DATALAYER:
-viewSource.addEventListener("click", function(){
+logo.addEventListener("click", function(){
 	dataLayer.push({
-		//VARIÁVEL:
-		"tipo_botao":"view_source",
-		//EVENTO:
-		"event":"disparo-personalizado"
-
+	'event': 'eventoTeste',
 	});
 });
